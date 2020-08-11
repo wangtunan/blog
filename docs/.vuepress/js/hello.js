5c671dcb97438b3e3a73affbb792c55b07a6b652
wangtunan <why583440138@gmail.com>
2020-08-11 21:57:38 +0800

    vuepress 导入代码修改

--- docs/.vuepress/js/hello.js
+++ docs/.vuepress/js/hello.js
@@ -1,8 +1,3 @@
 export function Hello () {
-<<<<<<< HEAD
-  // 导入代码片段代码
-  console.log('Hello, World!')
-=======
   console.log('Hello, Vuepress')
->>>>>>> f20052ce1cb3e84e7bdd81b48e6cc33578c78eb1
 }
