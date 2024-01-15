16967ac0c6261c43b221dfac589795171da2af85
wangtunan <why583440138@gmail.com>
2024-01-15 13:48:42 +0000

    Deploying to gh-pages from  @ 71baea88e8a1d1f76eb71d8868858bd2e270ff3c 🚀

---
+++ assets/index.html-unii8Zgk.js
@@ -0,0 +1,1 @@
+import{_ as t,o as a,c,a as e,b as n}from"./app-kez3bFl-.js";const o={},r=e("h1",{id:"简单",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#简单","aria-hidden":"true"},"#"),n(" 简单")],-1),s=[r];function _(d,i){return a(),c("div",null,s)}const l=t(o,[["render",_],["__file","index.html.vue"]]);export{l as default};
