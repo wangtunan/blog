9c4d2ffae8e16c088eb9f196739e7ee86ad03bf4
wangtunan <why583440138@gmail.com>
2024-05-20 14:48:35 +0000

    Deploying to gh-pages from  @ 0e33bace5491641d760e90f595f24e8b8c9da0d6 🚀

---
+++ assets/async.html-a89ec004.js
@@ -0,0 +1,1 @@
+import{_ as t,o as a,c,a as e,b as s}from"./app-3f58ab34.js";const n={},o=e("h1",{id:"异步组件",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#异步组件","aria-hidden":"true"},"#"),s(" 异步组件")],-1),r=e("p",null,"异步组件",-1),_=[o,r];function d(i,l){return a(),c("div",null,_)}const f=t(n,[["render",d],["__file","async.html.vue"]]);export{f as default};
